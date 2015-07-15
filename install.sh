ad1b05eccf7106a67bfdbfddf2f231b9007be117
Chris Daish <chrisdaish@gmail.com>
2015-07-15 08:21:27 +0100

    changes

--- install.sh
+++ install.sh
@@ -12,3 +12,7 @@ $LN -sf $HOME/.environment/vim/.vimrc $HOME/.vimrc
 # X
 $LN -sf $HOME/.environment/X/.Xresources $HOME/.Xresources
 $XRDB $HOME/.Xresources
+
+# i3
+$LN -sf $HOME/.environment/i3 $HOME/.i3
+$LN -sf $HOME/.environment/i3/i3status.conf $HOME/.i3status.conf
