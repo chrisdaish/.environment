3e075723f86d22de520d5eb96b387102dc2b6400
Chris Daish <chrisdaish@gmail.com>
2015-07-16 11:20:38 +0100

    Updates

--- .bash_profile
+++ .bash_profile
@@ -1,4 +1,6 @@
-for file in ~/.{bash_prompt,aliases,functions,path,extra,exports,dockerfunctions}; do
+#! /bin/bash
+
+for file in ~/.{bash_prompt,aliases,functions,path,extra,exports,corp,dockerfunctions}; do
   [[ -r "$file" ]] && [[ -f "$file" ]] && source "$file"
 done
 unset file
--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@
 LN=$(/usr/bin/which ln)
 XRDB=$(/usr/bin/which xrdb)
 
+# bash
+$LN -sf $HOME/.environment/.bash_profile $HOME/.bash_profile
+$LN -sf $HOME/.environment/.corp $HOME/.corp
+$LN -sf $HOME/.environment/.dockerfunctions $HOME/.dockerfunctions
+
 # vim
 $LN -sf $HOME/.environment/vim $HOME/.vim
 $LN -sf $HOME/.environment/vim/.vimrc $HOME/.vimrc
