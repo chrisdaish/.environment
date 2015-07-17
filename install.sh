8dd41fd9120b1ceaa8fa79a770722c537df2a111
Chris Daish <chrisdaish@gmail.com>
2015-07-17 08:41:25 +0100

    Added more env files

--- install.sh
+++ install.sh
@@ -6,18 +6,27 @@ LN=$(/usr/bin/which ln)
 XRDB=$(/usr/bin/which xrdb)
 
 # bash
+$LN -sf $HOME/.environment/.bash_profile $HOME/.bashrc
 $LN -sf $HOME/.environment/.bash_profile $HOME/.bash_profile
 $LN -sf $HOME/.environment/.corp $HOME/.corp
 $LN -sf $HOME/.environment/.dockerfunctions $HOME/.dockerfunctions
 
+# git
+$LN -sf $HOME/.environment/.gitconfig $HOME/.gitconfig
+
+# i3
+if [[ ! -L "$HOME/.i3" ]]; then
+  $LN -sf $HOME/.environment/i3 $HOME/.i3
+fi
+$LN -sf $HOME/.environment/i3/i3status.conf $HOME/.i3status.conf
+
 # vim
-$LN -sf $HOME/.environment/vim $HOME/.vim
+if [[ ! -L "$HOME/.vim" ]]; then
+  $LN -sf $HOME/.environment/vim $HOME/.vim
+fi
 $LN -sf $HOME/.environment/vim/.vimrc $HOME/.vimrc
 
 # X
 $LN -sf $HOME/.environment/X/.Xresources $HOME/.Xresources
 $XRDB $HOME/.Xresources
 
-# i3
-$LN -sf $HOME/.environment/i3 $HOME/.i3
-$LN -sf $HOME/.environment/i3/i3status.conf $HOME/.i3status.conf
