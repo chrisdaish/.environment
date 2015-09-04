27dde063ebd12e0ae94b609c8229035393f1a8cb
Chris Daish <chrisdaish@gmail.com>
2015-09-04 08:38:07 +0100

    Proxy script intelligence

--- bin/termProxy.sh
+++ bin/termProxy.sh
@@ -8,11 +8,21 @@ if [[ `uname` = 'Darwin' ]]; then
 
   HTTP_PROXY_HOSTNAME=`networksetup -getwebproxy "$SERVICE_NAME" | awk {'print $2'} | awk 'FNR == 2 {print}' | cut -d' ' -f2`
   HTTP_PROXY_PORT=`networksetup -getwebproxy "$SERVICE_NAME" | awk {'print $2'} | awk 'FNR == 3 {print}' | cut -d' ' -f2`
-  export http_proxy="$HTTP_PROXY_HOSTNAME:$HTTP_PROXY_PORT";
-  export HTTP_PROXY=$http_proxy
+  if [[ $HTTP_PROXY_HOSTNAME != '' ]]; then
+    export http_proxy="${HTTP_PROXY_HOSTNAME}:${HTTP_PROXY_PORT}";
+    export HTTP_PROXY=$http_proxy
+  else
+    unset http_proxy
+    unset HTTP_PROXY
+  fi
 
   HTTPS_PROXY_HOSTNAME=`networksetup -getsecurewebproxy "$SERVICE_NAME" | awk {'print $2'} | awk 'FNR == 2 {print}' |  cut -d' ' -f2`
   HTTPS_PROXY_PORT=`networksetup -getsecurewebproxy "$SERVICE_NAME" | awk {'print $2'} | awk 'FNR == 3 {print}' | cut -d' ' -f2`
-  export https_proxy="$HTTPS_PROXY_HOSTNAME:$HTTPS_PROXY_PORT";
-  export HTTPS_PROXY=$https_proxy
+  if [[ $HTTPS_PROXY_HOSTNAME != '' ]]; then
+    export https_proxy="${HTTPS_PROXY_HOSTNAME}:${HTTPS_PROXY_PORT}";
+    export HTTPS_PROXY=$https_proxy
+  else
+    unset https_proxy
+    unset HTTPS_PROXY
+  fi
 fi
