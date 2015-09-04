40ca0f927a1621117270ebedf1055ab51f72f3da
Chris Daish <chrisdaish@gmail.com>
2015-09-04 09:00:06 +0100

    Proxy script now sets git proxy

--- bin/termProxy.sh
+++ bin/termProxy.sh
@@ -9,20 +9,32 @@ if [[ `uname` = 'Darwin' ]]; then
   HTTP_PROXY_HOSTNAME=`networksetup -getwebproxy "$SERVICE_NAME" | awk {'print $2'} | awk 'FNR == 2 {print}' | cut -d' ' -f2`
   HTTP_PROXY_PORT=`networksetup -getwebproxy "$SERVICE_NAME" | awk {'print $2'} | awk 'FNR == 3 {print}' | cut -d' ' -f2`
   if [[ $HTTP_PROXY_HOSTNAME != '' ]]; then
+    # Terminal
     export http_proxy="${HTTP_PROXY_HOSTNAME}:${HTTP_PROXY_PORT}";
     export HTTP_PROXY=$http_proxy
+    # Git
+    git config --global http.proxy http://${HTTP_PROXY_HOSTNAME}:${HTTP_PROXY_PORT}
   else
+    # Terminal
     unset http_proxy
     unset HTTP_PROXY
+    # Git
+    git config --global --unset http.proxy
   fi
 
   HTTPS_PROXY_HOSTNAME=`networksetup -getsecurewebproxy "$SERVICE_NAME" | awk {'print $2'} | awk 'FNR == 2 {print}' |  cut -d' ' -f2`
   HTTPS_PROXY_PORT=`networksetup -getsecurewebproxy "$SERVICE_NAME" | awk {'print $2'} | awk 'FNR == 3 {print}' | cut -d' ' -f2`
   if [[ $HTTPS_PROXY_HOSTNAME != '' ]]; then
+    # Terminal
     export https_proxy="${HTTPS_PROXY_HOSTNAME}:${HTTPS_PROXY_PORT}";
     export HTTPS_PROXY=$https_proxy
+    # Git
+    git config --global https.proxy http://${HTTPS_PROXY_HOSTNAME}:${HTTPS_PROXY_PORT}
   else
+    # Terminal
     unset https_proxy
     unset HTTPS_PROXY
+    # Git
+    git config --global --unset https.proxy
   fi
 fi
